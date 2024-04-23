02a26c11b893714324329c726bf940231c96e00c
Alfonso Boizas Crespo <boizasal@gmail.com>
2024-04-23 14:03:18 +0200

    First commit

---
+++ Movie Radar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Movie Radar
+//
+//  Created by Alfonso Boizas Crespo on 23/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
